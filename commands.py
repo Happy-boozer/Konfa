adee03f79ca924da19aed6c98de99b17469efdc9
александр ильин <a9672938037@gmail.com>
2024-10-02 19:20:26 +0300

    cd function
    Signed-off-by: александр ильин <a9672938037@gmail.com>

--- commands.py
+++ commands.py
@@ -1,10 +1,23 @@
 import tarfile
+import os
+from contextlib import contextmanager
 def echo(*args):
     print(*args)
-    #return args
 
 def ls():
     tar = tarfile.open("nry.tar")
     namelist = tar.getnames()
     for file in namelist:
         print(file)
+
+def exit():
+    return True
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
