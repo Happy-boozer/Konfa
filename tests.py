e0af542e7f0910eb2c80f9cb44e7149968400d9f
Alex <a9672938037@gmail.com>
2024-11-12 17:31:32 +0300

    chown function test
    Signed-off-by: Alex <a9672938037@gmail.com>

--- tests.py
+++ tests.py
@@ -25,3 +25,13 @@ class TestShellEmulator(unittest.TestCase):
         emulator = ShellEmulator(os.path.join(self.test_dir, "fs.tar"))
         # Здесь, например, можно переопределить метод ls для проверки вывода
         emulator.ls()  # Ожидаем, что в выводе будет "test_dir"
+
+    def test_cd(self):
+        emulator = ShellEmulator(os.path.join(self.test_dir, "fs.tar"))
+        emulator.cd("test_dir")
+        self.assertEqual(os.getcwd(), os.path.join(emulator.fs_path, "test_dir"))
+
+    def test_chown(self):
+        # Пример теста команды chown
+        emulator = ShellEmulator(os.path.join(self.test_dir, "fs.tar"))
+        # Тут можно проверить изменение владельца, если есть доступ к пользователям
